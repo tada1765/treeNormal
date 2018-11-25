70a607c1424fea079ec22b7215b0091b0e23f43d
tada1765 <www.tada1765@gmail.com>
2018-11-25 14:41:16 +0800

    added c.file and h.file.

---
+++ src/treeNormal.h
@@ -0,0 +1,5 @@
+#ifndef _TREENORMAL_H
+#define _TREENORMAL_H
+
+
+#endif // _TREENORMAL_H
---
+++ test/test_treeNormal.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "treeNormal.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_treeNormal_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement treeNormal");
+}
